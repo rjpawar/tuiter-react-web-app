5370a1cd3720dfb31612fa28f948a9d7f2a3db61
rjpawar <rjpawar3003@gmail.com>
2022-10-23 14:16:19 -0400

    Removed tuiter folder from public directory

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -7,10 +7,10 @@ import HomeComponent from "./home";
 function Tuiter() {
     return (
         <div className="row mt-2">
-            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+            <div className="col-2 col-md-2 col-lg-2 col-xl-2">
                 <NavigationSidebar active="explore"/>
             </div>
-            <div className="col-10 col-md-10 col-lg-11 col-xl-10"
+            <div className="col-10 col-md-10 col-lg-10 col-xl-10"
                  style={{"position": "relative"}}>
                 <Routes>
                     <Route path="home" element={<HomeComponent/>}/>
